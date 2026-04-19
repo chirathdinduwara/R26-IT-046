fc4d3f253fdd23c1cfd8c1d759dbf07de688e01d
chirathdinduwara <tdchirathinduwara@gmail.com>
2026-04-19 22:49:07 +0530

    ui updates

--- Geonix-mobile/src/screens/home/HomeScreen.jsx
+++ Geonix-mobile/src/screens/home/HomeScreen.jsx
@@ -37,7 +37,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#9daeef",
+    backgroundColor: "#0042A5",
     padding: 16,
     gap: 20,
   },
